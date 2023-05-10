6d3bbaa1a164490e08a68f22759f51b7d053f637
Tom Miller <tommymillerr@outlook.com>
2023-05-10 09:39:09 +0300

    Initial Commit

---
+++ JobTracker/JobTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobTrackerApp.swift
+//  JobTracker
+//
+//  Created by Tom Miller on 10/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct JobTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
